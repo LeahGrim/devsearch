56e0a317e90793992ca2a387d2ffa4f69f992b8e
Leah Grim <grimchelate@gmail.com>
2023-08-21 11:14:06 -0500

    configuring github

---
+++ projects/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from .models import Project
+
+class ProjectForm(ModelForm):
+    class Meta: 
+        model = Project
+        fields = ['title', 'description', 'featured_image', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+        def __init__(self, *args, **kwargs):
+            super(ProjectForm, self).__init__(*args, **kwargs)
+
+            self.fields['title'].widget.attrs.update({'class': 'input'})
