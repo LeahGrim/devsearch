5bf25771f846ba2f312a4d31b303f03c68eda3f5
Leah Grim <grimchelate@gmail.com>
2023-08-24 11:44:24 -0500

    added debug messages on front end for user auth

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 # Create your views here.
@@ -17,7 +18,7 @@ def loginUser(request):
         try: 
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist')
+            messages.error(request, 'Username does not exist')
 
         user = authenticate(request, username= username, password=password)
         #this is sating if the user exists, direct user to profiles 
@@ -25,12 +26,13 @@ def loginUser(request):
             login(request, user)
             return redirect('profiles')
         else: 
-            print('Username or Password is incorrect!')
+           messages.error(request, 'Username or Password is incorrect!')
         
     return render(request, 'users/login_register.html')
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, 'User was successfully logged out!')
     return redirect('login')
 
 def profiles(request):
