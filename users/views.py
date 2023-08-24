effb0f22c30ded4b967cf3f12d4ab7523d791d6f
Leah Grim <grimchelate@gmail.com>
2023-08-24 10:48:27 -0500

    added login to views, and mapped it through to navbar and users urls

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Profile
 # Create your views here.
+def loginPage(request):
+    return render(request, 'users/login_register.html')
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
