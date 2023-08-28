75d4bf24a0c3660dfc387fc2e5eab5d2ed759afa
Leah Grim <grimchelate@gmail.com>
2023-08-28 11:31:14 -0500

    adding changes from the weekend

--- users/views.py
+++ users/views.py
@@ -71,3 +71,12 @@ def userProfile(request, pk):
     otherSkills = profile.skill_set.filter(description="")
     context = {'profile': profile, 'topSkills': topSkills, "otherSkills": otherSkills }
     return render(request, 'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    context={'profile': profile, 'skills': skills,
+             'projects': projects}
+    return render(request, 'users/account.html', context)
