564934e3d055808672eff7fd36986dcfee6caf3a
Leah Grim <grimchelate@gmail.com>
2023-08-29 14:27:28 -0500

    added tags to search query on projects

--- projects/views.py
+++ projects/views.py
@@ -12,10 +12,13 @@ def projects(request):
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
 
-    projects = Project.objects.filter(
+    tags= Tag.objects.filter(name__icontains= search_query)
+
+    projects = Project.objects.distinct().filter(
         Q(title__icontains=search_query)|
         Q(description__icontains= search_query)|
-        Q(owner__name__icontains= search_query) 
+        Q(owner__name__icontains= search_query)|
+        Q(tags__in=tags)
     )
     context = {'projects': projects, 'search_query':search_query}
     return render(request, 'projects/projects.html', context )
