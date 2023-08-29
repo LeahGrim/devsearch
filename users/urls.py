7ca1287799db9d6ee806868a3192c2ab8ed8c281
Leah Grim <grimchelate@gmail.com>
2023-08-28 20:02:07 -0500

    added url path to create-skill

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('account/', views.userAccount, name="account"),
     path('edit-account/', views.editAccount, name="edit-account"),
+    path('create-skill/', views.createSkill, name="create-skill"),
     
 ]
