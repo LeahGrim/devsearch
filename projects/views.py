1f4123ec9b75e277cc7f411e6dea96f51ae1c37c
Leah Grim <grimchelate@gmail.com>
2023-08-28 19:59:09 -0500

    created skill_form.html for user to create skill

--- projects/views.py
+++ projects/views.py
@@ -31,7 +31,8 @@ def createProject(request):
 
 @login_required(login_url= "login")
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile 
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if request.method == 'POST':
@@ -45,7 +46,8 @@ def updateProject(request, pk):
 
 @login_required(login_url= "login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile 
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
