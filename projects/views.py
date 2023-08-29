f1bc9c07035fe1f837036b332bc0f3ee4ed19771
Leah Grim <grimchelate@gmail.com>
2023-08-29 14:58:14 -0500

    added pagination functionality

--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Project, Tag
 from .forms import ProjectForm
-from . utils import searchProjects
+from .utils import searchProjects
 
 def projects(request): 
-    
     projects, search_query= searchProjects(request)
 
-    context = {'projects': projects, 'search_query':search_query}
+    page = request.GET.get('page')
+    results = 3 
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
+
+
+    context = {'projects': projects, 'search_query':search_query, 'paginator': paginator }
     return render(request, 'projects/projects.html', context )
 
 def project (request, pk):
